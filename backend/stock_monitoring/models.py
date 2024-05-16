e4e0909aafb153d726fbba2d332d6f2292a5e320
Som Pratap Singh <singhsom759@gmail.com>
2024-05-16 15:32:36 +0530

    initialised backend

---
+++ backend/stock_monitoring/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class StockSymbol(models.Model):
+    symbol = models.CharField(max_length=10, unique=True)
+    name = models.CharField(max_length=100, blank=True, null=True)
+    
+    def __str__(self):
+        return self.symbol
+
+class Watchlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, blank=True, null=True)
+    symbols = models.ManyToManyField(StockSymbol)
+    
+    def __str__(self):
+        return f"{self.user.username}'s Watchlist"
